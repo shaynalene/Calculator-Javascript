6964741e9f1aac3534df67ea3567bb69783318bb
shaynalene <shaynalenep@gmail.com>
2024-06-04 10:55:46 +0800

    Create basic math operator functions and edit js file
    Create basic math operator functions and changed the script.js file.

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+/*
+let calc = new Calculator;
+
+function Calculator(){
+    this.operations ={
+        "+" = (a,b) => a+b,
+        "-" = (a,b) => a-b,
+        "*" = (a,b) => a*b,
+        "/" = (a,b) => a/b,
+    }
+}
+*/
